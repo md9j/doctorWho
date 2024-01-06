b625a6d59cea0266f27e0a60b061d96bcd058b4b
Micaheal Johnston <36141218+md9j@users.noreply.github.com>
2024-01-05 18:56:15 -0700

    Add deployWhodabase.sh script for deploying Whodabase container

---
+++ deployWhodabase.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ssh root@45.79.115.237 'bash -s'
+
+docker pull ghcr.io/md9j/doctor-who/whodabase:latest
+docker stop whodabase 
+docker rm whodabase
+docker run -d -p 3000:3000 --name whodabase  ghcr.io/md9j/doctor-who/whodabase:latest
