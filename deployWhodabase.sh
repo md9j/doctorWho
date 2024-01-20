faba3d566953bd05aee9f1c32e1c7ad7fca11633
Micaheal Johnston <36141218+md9j@users.noreply.github.com>
2024-01-19 20:02:48 -0700

    Update deployWhodabase.sh script to include environment file path

--- deployWhodabase.sh
+++ deployWhodabase.sh
@@ -5,4 +5,4 @@ ssh root@45.79.115.237 'bash -s'
 docker pull ghcr.io/md9j/doctor-who/whodabase:latest
 docker stop whodabase 
 docker rm whodabase
-docker run -d -p 3000:3000 --name whodabase  ghcr.io/md9j/doctor-who/whodabase:latest
+docker run -d -p 3000:3000 --env-file /etc/doctorwhodabase/.env --name whodabase ghcr.io/md9j/doctor-who/whodabase:latest
